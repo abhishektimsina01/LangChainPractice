9f9c0d92c29c4b6cddfbc5746118e9e80d8a6d5d
Abhishek_Timsina <timsinaabhishek1@gmail.com>
2026-01-10 21:08:59 +0545

    ffixing error

--- tools_toolsCalling.py
+++ tools_toolsCalling.py
@@ -23,7 +23,7 @@ search_tool = DuckDuckGoSearchRun()
 results = search_tool.invoke("Abhishek Timsina")
 # print(results)
 
-print("Custom tools")
+# print("Custom tools")
 @tool
 def multiplyTool(a : int, b : int) -> int:
     # docs string => llm can understand waht the function can really do
@@ -31,11 +31,11 @@ def multiplyTool(a : int, b : int) -> int:
     return a*b
 
 result = multiplyTool.invoke({'a': 5, 'b':5})
-print(result)
+# print(result)
 
-print(multiplyTool.name)
-print(multiplyTool.description)
-print(multiplyTool.args)
+# print(multiplyTool.name)
+# print(multiplyTool.description)
+# print(multiplyTool.args)
 
 class MultiplyInput(BaseModel):
     a : int = Field(description="first number to add", required = True)
@@ -51,7 +51,7 @@ multiply_tool = StructuredTool.from_function(
     description="multiply two number",
     args_schema=MultiplyInput
 )
-print(multiply_tool.invoke({'a' : 1, 'b' : 1}))
+# print(multiply_tool.invoke({'a' : 1, 'b' : 1}))
 
 
 # tool binding = connecting llm and tool
@@ -61,15 +61,17 @@ llm = ChatGroq(
 )
 
 llm_with_tools = llm.bind_tools([multiplyTool])
-print(llm_with_tools.invoke("Hi, how are you?"))
+# print(llm_with_tools.invoke("Hi, how are you?"))
 print("---------------------------------------------")
 
 # it just suggest us the tool we can use and also the arguements we have to pass while using tools
 query = HumanMessage("can you multiply 3 with 10")
 messages = [query]
 result = llm_with_tools.invoke(messages)
+print(result)
 messages.append(result)
 tool_result = multiplyTool.invoke(result.tool_calls[0])
+print(tool_result)
 messages.append(tool_result)
 print(messages)
 print(llm_with_tools.invoke(messages).content)  
